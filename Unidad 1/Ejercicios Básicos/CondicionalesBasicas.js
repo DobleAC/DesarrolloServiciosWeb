d72f128e556c68b542e6cef2b46fb69066033236
DobleAC <95879539+DobleAC@users.noreply.github.com>
2024-08-31 15:45:51 -0700

    Ejercicios Básicos
    Desarrollo de ejercicio básico en JavaScript en equipo.

---
+++ Unidad 1/Ejercicios Básicos/CondicionalesBasicas.js
@@ -0,0 +1,9 @@
+function mayorDeEdad (edad){
+    if (edad >= 18)
+    {
+        return "Eres mayor de edad";
+    }else{
+            return "Eres menor de edad";
+        }
+    }
+console.log(mayorDeEdad(12));
---
+++ Unidad 1/Ejercicios Básicos/VariablesyOperacionesB.js
@@ -0,0 +1,12 @@
+var a=5;
+var b=5;
+
+var resultado= a+b;
+var resultado1= a-b;
+var resultado2= a*b;
+var resultado3= a/b;
+
+console.log('El resultado de la suma es: '+resultado);
+console.log('El resultado de la resta es: '+resultado1);
+console.log('El resultado de la muñtiplicacion es: '+resultado2);
+console.log('El resultado de la division es: '+resultado3);
